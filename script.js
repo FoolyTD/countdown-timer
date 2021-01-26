890d5182983d704bd4d9807e3d6fb730312639cf
FoolyTD <74512874+FoolyTD@users.noreply.github.com>
2021-01-25 20:32:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEL = document.getElementById('hours');
 const minutesEL = document.getElementById('minutes');
 const secondsEL = document.getElementById('seconds');
 
-const departingDate = '1 Jan 2021';
+const departingDate = '7 June 2021';
 
 function formatTime(time) {
     if(time < 10) {
